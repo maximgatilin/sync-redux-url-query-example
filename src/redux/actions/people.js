15bad36706f7b7476a25a9c7d5ce10be804b4724
Gatilin Maxim <gatilin222@gmail.com>
2018-08-04 17:51:51 +0300

    Push values to url after actions

--- src/redux/actions/people.js
+++ src/redux/actions/people.js
@@ -2,5 +2,9 @@ import { CHANGE_PEOPLE_AGE_FILTER } from './actionTypes';
 
 export const changePeopleAgeFilter = (filter) => ({
   type: CHANGE_PEOPLE_AGE_FILTER,
-  payload: {filter}
+  payload: {filter},
+  pushToUrl: {
+    minAge: filter[0],
+    maxAge: filter[1]
+  }
 });
--- src/redux/middlewares/urlSync.js
+++ src/redux/middlewares/urlSync.js
@@ -1,13 +1,17 @@
-// import store from '../index';
-//
-// store.dispatch(push({
-//   pathname: "/people",
-//   query: {
-//     foo: "bar",
-//     baz: "3"
-//   }
-// }));
+import {replace} from 'react-router-redux';
 
 export default store => next => action => {
+  if (action.pushToUrl) {
+    const state = store.getState();
+
+    store.dispatch(replace({
+      pathname: state.routing.locationBeforeTransitions.pathname,
+      query: {
+        ...state.routing.locationBeforeTransitions.query,
+        ...action.pushToUrl
+      }
+    }));
+  }
+
   return next(action);
 }
