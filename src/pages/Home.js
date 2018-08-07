1aa6efaa8840aa158092777f72da57b50a2fe78c
Gatilin Maxim <gatilin222@gmail.com>
2018-08-07 08:28:17 +0300

    Grammar fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,14 +7,14 @@ export default class Home extends Component {
       <div style={{fontSize: 20, lineHeight: 1.8}}>
         Hi, stranger!
         <br/>
-        I've created this tiny app to show you how a Redux store can be connected with URL queries.
+        This tiny app was created to show you how a Redux store can be connected with URL queries.
         <br/>
         You can go to the  <Link to={"/products"}>products</Link> or <Link to={"/products"}>users</Link> page and change some filters.
         <br/>
         The filters will be saved in the URL and when this URL is loaded filters will be persisted.
         <br/>
         <br/>
-        Source code is available on <a href="https://github.com/maximgatilin/sync-redux-url-query-example" target="_blank">Github</a>
+        The source code is available on <a href="https://github.com/maximgatilin/sync-redux-url-query-example" target="_blank">Github</a>
       </div>
     );
   }
